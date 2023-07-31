3e2eea6d3c605f5dac6ec2fcb21c7bcf9a95d075
Antoine <a.heidet90400@gmail.com>
2023-07-31 22:02:25 +0200

    ajout useEffect pour rechargement au changement de page

--- src/PDF_Viewer/index.jsx
+++ src/PDF_Viewer/index.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import samplePdf from './starter.pdf';
+import samplePdf from './logo.pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
@@ -8,69 +8,86 @@ import styled from 'styled-components';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function PDFViewer () {
-    const [numPages, setNumPages] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [selectedText, setSelectedText] = useState('');
-    const handleDocumentLoadSuccess = ({ numPages }) => {
-        setNumPages(numPages);
-    };
+export default function PDFViewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedText, setSelectedText] = useState('');
 
-    const handleTextSelection = () => {
-        const selection = window.getSelection();
-        if (selection) {
-            setSelectedText(selection.toString());
-        } else {
-            setSelectedText('');
-        }
-    };
+  useEffect(() => {
+    // Réinitialiser les états lorsque le document PDF change
+    setNumPages(null);
+    setCurrentPage(1);
+    setSelectedText('');
+  }, [samplePdf]); // Ajoutez samplePdf en tant que dépendance de l'effet
 
-    const goToPreviousPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+  const handleDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
 
-    const goToNextPage = () => {
-        if (currentPage < numPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
+  const handleTextSelection = () => {
+    const selection = window.getSelection();
+    if (selection) {
+      setSelectedText(selection.toString());
+    } else {
+      setSelectedText('');
+    }
+  };
 
-    return (
-        <PdfViewer>
-               <div>
-                    <button onClick={goToPreviousPage} disabled={currentPage === 1}>Page précédente</button>
-                    <button onClick={goToNextPage} disabled={currentPage === numPages}>Page suivante</button>
-                </div>
-            <Document file={samplePdf} onLoadSuccess={handleDocumentLoadSuccess}>
-                {Array.from(new Array(numPages), (_, index) => index).map((pageIndex) => (
-                    <Page
-                        key={`page_${pageIndex + 1}`}
-                        pageNumber={pageIndex + 1}
-                        onClick={(selection) => handleTextSelection(pageIndex, selection)}
-                    />
-                ))}
-            </Document>
-            <TextSelectView>
-                <p>Nombre de pages : {numPages}</p>
-                <strong>Texte sélectionné : {selectedText}</strong>
-            </TextSelectView>
-        </PdfViewer>
-    );
-};
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
+  const goToNextPage = () => {
+    if (currentPage < numPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
+  return (
+    <PdfViewer>
+      <ToolbarButton>
+        <button onClick={goToPreviousPage} disabled={currentPage === 1}>
+          Page précédente
+        </button>
+        <button onClick={goToNextPage} disabled={currentPage === numPages}>
+          Page suivante
+        </button>
+      </ToolbarButton>
+      <Viewer>
+        <Document file={samplePdf} onLoadSuccess={handleDocumentLoadSuccess}>
+            <Page
+            pageNumber={currentPage}
+            onClick={handleTextSelection}
+            />
+        </Document>
+        <TextSelectView>
+            <p>Nombre de pages : {numPages}</p>
+            <strong>Texte sélectionné : {selectedText}</strong>
+        </TextSelectView>
+      </Viewer>
+    </PdfViewer>
+  );
+}
 
 const TextSelectView = styled.div`
-    width: 30%;
-    height: 500px;
-    overflow: auto;
+  width: 30%;
+  height: 500px;
+  overflow: auto;
+`;
+
+const ToolbarButton = styled.div`
+  text-align: center;
+`;
+
+
+const Viewer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const PdfViewer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
+  padding-top: 2em;
 `;
