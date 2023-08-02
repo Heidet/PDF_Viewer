adcee9fec7fcb81d2612cf6fae00750eb6fcd76d
Heidet <a.heidet90400@gmail.com>
2023-08-02 17:08:31 +0200

    mise en place de l'input pour saisir le numéros de page

--- src/PDF_Viewer/index.jsx
+++ src/PDF_Viewer/index.jsx
@@ -1,14 +1,13 @@
 import "../App.css";
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import Drop from "./Drop";
 import { Document, Page, pdfjs, Outline} from "react-pdf";
 import { PDFDocument, rgb } from "pdf-lib";
 import { blobToURL } from "../utils/Utils";
-import PagingControl from "../components/PagingControl";
 import { AddSigDialog } from "../components/AddSigDialog";
 import DraggableSignature from "../components/DraggableSignature";
 import DraggableText from "../components/DraggableText";
 import dayjs from "dayjs";
+import Drop from "./Drop";
 import styled from 'styled-components';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -23,9 +22,7 @@ import { faSignature } from '@fortawesome/free-solid-svg-icons';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 pdfjs.GlobalWorkerOptions.useWorkerFetch = true; // Add this line to disable worker-based fetching
@@ -58,9 +55,13 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [searchText, setSearchText] = useState('');
 
-  const onItemClick = ( itemPageNumber ) => {
-    console.log(itemPageNumber)
-    setPageNum(itemPageNumber.pageNumber);
+  const onSearchPage = (itemPageNumber) => {
+    const parsedPageNumber = parseInt(itemPageNumber, 10); 
+    if (!isNaN(parsedPageNumber) && parsedPageNumber > 0 && parsedPageNumber <= totalPages) {
+      setPageNum(parsedPageNumber);
+    } else {
+      console.error('Invalid page number');
+    }
   }
 
   const textRenderer = useCallback(
@@ -77,7 +78,6 @@ function App() {
   }
 
   useEffect(() => {
-    console.log(pageNum)
     setCurrentPage(1);
     setSelectedText('');
   }, []);
@@ -166,15 +166,35 @@ function App() {
                       > <FontAwesomeIcon icon={faDownload} /></Button>
                     </>
                   ) : null}
-                  <PagingControl
+                      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        {/* <Button
+                          size="small"
+                          style={{marginRight: 8, color: 'white'}}
+                          onClick={() => setPageNum(pageNum - 1)}
+                          disabled={pageNum-1===-1}
+                        > Précedent </Button> */}
+                        {/* <div style={styles.pageInfo}>
+                          Page: {pageNum + 1}/{totalPages}
+                        </div> */}
+                        <div style={{color: 'white',fontSize: 14}}>
+                          <input style={{width: '20px'}} onChange={(e) => onSearchPage(e.target.value)} defaultValue={pageNum}></input>/{totalPages}
+                        </div>
+                        {/* <Button
+                          size="small"
+                          style={{marginRight: 8, color: 'white'}}
+                          onClick={() => setPageNum(pageNum + 1)}
+                          disabled={pageNum+1>totalPages-1}
+                        > Suivant </Button> */}
+                      </div>
+                  {/* <PagingControl
                     style={{color: 'white'}}
                     pageNum={pageNum}
                     setPageNum={setPageNum}
                     totalPages={totalPages}
-                  />
-                  <div>
-                    <label htmlFor="search">Search:</label>
-                    <input type="search" id="search" value={searchText} onChange={onChange} />
+                  /> */}
+                  <div style={{marginLeft: 8}}>
+                    <label htmlFor="search">Rechercher: </label>
+                    <input style={{marginLeft: 2}} type="search" id="search" value={searchText} onChange={onChange} />
                   </div>
                 </Toolbar>
               </AppBar>
@@ -204,11 +224,11 @@ function App() {
                       166 -
                       position.offsetX -
                       documentRef.current.offsetLeft;
-
                     const newY =
                       (y * originalHeight) / documentRef.current.clientHeight;
                     const newX =
                       (x * originalWidth) / documentRef.current.clientWidth;
+
                     const pdfDoc = await PDFDocument.load(pdf);
                     const pages = pdfDoc.getPages();
                     const firstPage = pages[pageNum];
@@ -255,10 +275,8 @@ function App() {
                       (x * originalWidth) / documentRef.current.clientWidth;
 
                     const pdfDoc = await PDFDocument.load(pdf);
-
                     const pages = pdfDoc.getPages();
                     const firstPage = pages[pageNum];
-
                     const pngImage = await pdfDoc.embedPng(signatureURL);
                     const pngDims = pngImage.scale( scale * .3);
 
@@ -271,7 +289,7 @@ function App() {
 
                     if (autoDate) {
                       firstPage.drawText(
-                        `Signed ${dayjs().format(
+                        `Signé ${dayjs().format(
                           "M/d/YYYY HH:mm:ss ZZ"
                         )}`,
                         {
@@ -285,7 +303,6 @@ function App() {
 
                     const pdfBytes = await pdfDoc.save();
                     const blob = new Blob([new Uint8Array(pdfBytes)]);
-
                     const URL = await blobToURL(blob);
                     setPdf(URL);
                     setPosition(null);
@@ -300,7 +317,7 @@ function App() {
                   setTotalPages(data.numPages);
                 }}
               >
-                <Outline onItemClick={onItemClick} />
+                {/* <Outline onItemClick={(e) => onSearchPage(e.pageNumber)} /> */}
                 <Page
                   pageNumber={pageNum + 1}
                   onClick={handleTextSelection}
@@ -311,13 +328,6 @@ function App() {
                     setPageDetails(data);
                   }}
                 >
-                  {searchResults.map((highlight, index) => (
-                    <HighlightBox
-                      key={index}
-                      position={highlight.position.boundingRect}
-                      color="yellow"
-                    />
-                  ))}
                 </Page>
               </Document>
             </div>
@@ -337,26 +347,10 @@ function App() {
 
 export default App;
 
-const HighlightBox = styled.div`
-  position: absolute;
-  pointer-events: none;
-  background-color: ${props => props.color};
-  opacity: 0.5;
-  z-index: 1;
-  top: ${props => props.position.top}px;
-  left: ${props => props.position.left}px;
-  width: ${props => props.position.width}px;
-  height: ${props => props.position.height}px;
-`;
-
 const TextSelectView = styled.div`
   width: 30%;
   height: 500px;
   overflow: auto;
-`;
-
-const ToolbarButton = styled.div`
-  text-align: center;
 `;
 
 const Viewer = styled.div`
